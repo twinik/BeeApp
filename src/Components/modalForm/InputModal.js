e378ad20cc23be219a341bd1c134a2b9e6f0c393
twinik <tobiwinik@gmail.com>
2021-10-01 11:39:54 -0300

    Update InputModal.js

--- src/Components/modalForm/InputModal.js
+++ src/Components/modalForm/InputModal.js
@@ -2,17 +2,17 @@ import React from "react";
 import { Alert, Modal, StyleSheet, Text, Pressable, View } from "react-native";
 import { Input } from "react-native-elements";
 import FormItem from "../Forms/FormItem";
-import MyText from '../MyText'
-export default function InputModal({titulo,placeholder,icon,control}) {
+import MyText from "../MyText";
+export default function InputModal({ titulo, placeholder, icon, control }) {
   return (
     <>
-      <MyText text={titulo} style={{ letterSpacing: 2 }} fontStyle="Bold"/>
+      <MyText text={titulo} style={{ letterSpacing: 2 }} fontStyle="Bold" />
       <FormItem control={control} label="asunto">
-      <Input
-        placeholder={placeholder}
-        leftIcon={icon}
-        style={{paddingLeft:10}}
-      />
+        <Input
+          placeholder={placeholder}
+          leftIcon={icon}
+          style={{ paddingLeft: 10 }}
+        />
       </FormItem>
     </>
   );

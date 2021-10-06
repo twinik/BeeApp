f0cb893276c5b26cce8b2c3f41b221b38ba57d64
galo <67323030+gfalotico@users.noreply.github.com>
2021-10-06 17:39:31 -0300

    Update Firebase.js

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD50bVrMQzeyCcAZ3GDDP8dzT6MYZ1z8nA",
-  authDomain: "servbee-1f3a6.firebaseapp.com",
-  projectId: "servbee-1f3a6",
-  storageBucket: "servbee-1f3a6.appspot.com",
-  messagingSenderId: "181242696518",
-  appId: "1:181242696518:web:93652e010acab818580b47"
+  apiKey: "*",
+  authDomain: "*",
+  projectId: "*",
+  storageBucket: "*",
+  messagingSenderId: "*",
+  appId: "*"
 };
 
 // Initialize Firebase

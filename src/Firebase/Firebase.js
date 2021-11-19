636b88cda2b8f3e5ebc9d56f250ffea25df0e863
twinik <tobiwinik@gmail.com>
2021-11-19 11:23:09 -0300

    Update Firebase.js

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase'
+import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "*",
-  authDomain: "*",
-  projectId: "*",
-  storageBucket: "*",
-  messagingSenderId: "*",
-  appId: "*"
+  apiKey: "AIzaSyD50bVrMQzeyCcAZ3GDDP8dzT6MYZ1z8nA",
+  authDomain: "servbee-1f3a6.firebaseapp.com",
+  projectId: "servbee-1f3a6",
+  storageBucket: "servbee-1f3a6.appspot.com",
+  messagingSenderId: "181242696518",
+  appId: "1:181242696518:web:93652e010acab818580b47",
 };
 
 // Initialize Firebase
-const Firebase = firebase.initializeApp(firebaseConfig)
+const Firebase = firebase.initializeApp(firebaseConfig);
 
-export default Firebase
+export default Firebase;
